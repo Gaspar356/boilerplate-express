1e8d2496c406927e666c0d4ef794449a4959bd73
Gaspar356 <gasparandres2002@gmail.com>
2023-09-13 01:57:24 -0300

    dios

--- myApp.js
+++ myApp.js
@@ -1,8 +1,33 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
+
+console.log("Hello World");
 
 
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
+app.use("/public",express.static(__dirname + "/public" ));
+
+const express = require('express');
+
+
+app.get('/json', (req, res) => {
+  const messageStyle = process.env.MESSAGE_STYLE || 'uppercase';
+
+  if (messageStyle === 'uppercase') {
+    res.json({ message: 'HELLO JSON' });
+  } else {
+    res.json({ message: 'Hello json' });
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 
 
